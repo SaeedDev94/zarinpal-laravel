b4a13d7fb2e8f058f3665ccb5936b5e240ddbecb
SaeedPooyanfar <saeedp47@gmail.com>
2017-08-16 17:17:50 +0430

    removed register method in ZarinpalServiceProvider

--- src/Laravel/ZarinpalServiceProvider.php
+++ src/Laravel/ZarinpalServiceProvider.php
@@ -3,26 +3,9 @@
 namespace Zarinpal\Laravel;
 
 use Illuminate\Support\ServiceProvider;
-use Zarinpal\Drivers\SoapDriver;
-use Zarinpal\Zarinpal;
 
 class ZarinpalServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the service provider.
-     *
-     * @return \Zarinpal\Zarinpal
-     */
-    public function register()
-    {
-        $this->app->singleton('Zarinpal', function () {
-            $merchantID = config('Zarinpal.merchantID', 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX');
-            $driver = new SoapDriver();
-
-            return new Zarinpal($merchantID, $driver);
-        });
-    }
-
     /**
      * Publish the plugin configuration.
      */
