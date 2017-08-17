1de660750f7ae50a54afefa6b45eb0033314dc2e
SaeedPooyanfar <saeedp47@gmail.com>
2017-08-17 23:28:44 +0430

    replace tab with spaces in ZarinpalServiceProvider class

--- src/Laravel/ZarinpalServiceProvider.php
+++ src/Laravel/ZarinpalServiceProvider.php
@@ -14,29 +14,29 @@ class ZarinpalServiceProvider extends ServiceProvider
      *
      * @return Zarinpal\Zarinpal
      */
-	public function register()
-	{
-		$this->app->singleton('Zarinpal', function () {
-			$merchantID = (string) config('zarinpal.merchantID', 'test');
-			$driver = (string) config('zarinpal.driver', 'Rest');
-			$debug = (bool) config('zarinpal.debug', '0');
-			if($debug) {
-				$merchantID = 'test';
-				$driver = new SoapDriver();
-			}
-			else {
-				switch ($driver) {
-					case 'Soap':
-						$driver = new SoapDriver();
-						break;
-					default:
-						$driver = new RestDriver();
-						break;
-				}
-			}
-			return new Zarinpal($merchantID, $driver, $debug);
-		});
-	}
+    public function register()
+    {
+        $this->app->singleton('Zarinpal', function () {
+            $merchantID = (string) config('zarinpal.merchantID', 'test');
+            $driver = (string) config('zarinpal.driver', 'Rest');
+            $debug = (bool) config('zarinpal.debug', '0');
+            if($debug) {
+                $merchantID = 'test';
+                $driver = new SoapDriver();
+            }
+            else {
+                switch ($driver) {
+                    case 'Soap':
+                        $driver = new SoapDriver();
+                        break;
+                    default:
+                        $driver = new RestDriver();
+                        break;
+                }
+            }
+            return new Zarinpal($merchantID, $driver, $debug);
+        });
+    }
 
     /**
      * Publish the plugin configuration.
