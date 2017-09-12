189e6fba5c5f9f66d50f620e26d73407f2b192df
SaeedPooyanfar <saeedp47@gmail.com>
2017-09-12 18:29:35 +0430

    DRY rule for clients!

--- src/Clients/GuzzleClient.php
+++ src/Clients/GuzzleClient.php
@@ -5,7 +5,7 @@ namespace Zarinpal\Clients;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
-class GuzzleClient
+class GuzzleClient extends BaseClient
 {
     public $baseUrl;
 
@@ -13,60 +13,6 @@ class GuzzleClient
     {
         $sub = ($sandbox) ? 'sandbox' : 'www';
         $this->baseUrl = 'https://'.$sub.'.zarinpal.com/pg/rest/WebGate/';
-    }
-
-    /**
-     * Request new payment.
-     *
-     * @param  array $input
-     * @param  bool  $extra
-     *
-     * @return array
-     */
-    public function request($input, $extra)
-    {
-        $uri = ($extra) ? 'PaymentRequestWithExtra.json' : 'PaymentRequest.json';
-
-        return $this->sendRequest($uri, $input);
-    }
-
-    /**
-     * Verify the payment.
-     *
-     * @param  array $input
-     * @param  bool  $extra
-     *
-     * @return array
-     */
-    public function verify($input, $extra)
-    {
-        $uri = ($extra) ? 'PaymentVerificationWithExtra.json' : 'PaymentVerification.json';
-
-        return $this->sendRequest($uri, $input);
-    }
-
-    /**
-     * Extends authority token lifetime.
-     *
-     * @param  array $input
-     *
-     * @return array
-     */
-    public function refreshAuthority($input)
-    {
-        return $this->sendRequest('RefreshAuthority.json', $input);
-    }
-
-    /**
-     * Get unverified transactions.
-     *
-     * @param  array $input
-     *
-     * @return array
-     */
-    public function unverifiedTransactions($input)
-    {
-        return $this->sendRequest('UnverifiedTransactions.json', $input);
     }
 
     /**
@@ -80,6 +26,7 @@ class GuzzleClient
      */
     public function sendRequest($uri, $input)
     {
+        $uri .= '.json';
         try {
             $client = new Client(['base_uri' => $this->baseUrl]);
             $response = $client->request('POST', $uri, ['json' => $input]);
--- src/Clients/SoapClient.php
+++ src/Clients/SoapClient.php
@@ -5,7 +5,7 @@ namespace Zarinpal\Clients;
 use SoapClient as Client;
 use SoapFault;
 
-class SoapClient
+class SoapClient extends BaseClient
 {
     public $baseUrl;
 
@@ -13,60 +13,6 @@ class SoapClient
     {
         $sub = ($sandbox) ? 'sandbox' : 'www';
         $this->baseUrl = 'https://'.$sub.'.zarinpal.com/pg/services/WebGate/wsdl';
-    }
-
-    /**
-     * Request new payment.
-     *
-     * @param  array $input
-     * @param  bool  $extra
-     *
-     * @return array
-     */
-    public function request($input, $extra)
-    {
-        $uri = ($extra) ? 'PaymentRequestWithExtra' : 'PaymentRequest';
-
-        return $this->sendRequest($uri, $input);
-    }
-
-    /**
-     * Verify the payment.
-     *
-     * @param  array $input
-     * @param  bool  $extra
-     *
-     * @return array
-     */
-    public function verify($input, $extra)
-    {
-        $uri = ($extra) ? 'PaymentVerificationWithExtra' : 'PaymentVerification';
-
-        return $this->sendRequest($uri, $input);
-    }
-
-    /**
-     * Extends authority token lifetime.
-     *
-     * @param  array $input
-     *
-     * @return array
-     */
-    public function refreshAuthority($input)
-    {
-        return $this->sendRequest('RefreshAuthority', $input);
-    }
-
-    /**
-     * Get unverified transactions.
-     *
-     * @param  array $input
-     *
-     * @return array
-     */
-    public function unverifiedTransactions($input)
-    {
-        return $this->sendRequest('UnverifiedTransactions', $input);
     }
 
     /**
