0b7fce63257c560c4fdacff37fd14d160ee28412
SaeedPooyanfar <saeedp47@gmail.com>
2021-01-26 13:18:04 +0330

    Remove SoapClient as API v4 only support REST

--- src/ZarinpalServiceProvider.php
+++ src/ZarinpalServiceProvider.php
@@ -4,7 +4,6 @@ namespace Zarinpal;
 
 use Illuminate\Support\ServiceProvider;
 use Zarinpal\Clients\GuzzleClient;
-use Zarinpal\Clients\SoapClient;
 
 class ZarinpalServiceProvider extends ServiceProvider
 {
@@ -23,12 +22,11 @@ class ZarinpalServiceProvider extends ServiceProvider
     private function getInstance()
     {
         $merchantID = (string) config('zarinpal.merchantID', 'test');
-        $clientType = (string) config('zarinpal.client', 'Guzzle');
         $lang = (string) config('zarinpal.lang', 'fa');
         $sandbox = (bool) config('zarinpal.sandbox', '0');
         $zaringate = (bool) config('zarinpal.zaringate', '0');
         $zaringatePSP = (string) config('zarinpal.zaringate_psp', '');
-        $client = ($clientType === 'Soap') ? new SoapClient($sandbox) : new GuzzleClient($sandbox);
+        $client = new GuzzleClient($sandbox);
         return new Zarinpal($merchantID, $client, $lang, $sandbox, $zaringate, $zaringatePSP, true);
     }
 }
